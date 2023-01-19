be4be217b0e94626601e212cf361bde53f25ec1d
Okellothomas <thomasolal33@gmail.com>
2023-01-19 16:49:15 +0300

    myVariables.class

---
+++ src/Introduction/variables.kt
@@ -0,0 +1,28 @@
+package Introduction
+
+    fun main(){
+        /**
+         * Variables in java.
+         */
+
+        var myInt: Int = 200
+        var alsoMyInt = 300
+        var myLong: Long = 200102010201L
+        var alsoMyLong = 20201020102010L
+        var myByte: Byte = 20
+        var alsoMyByte = 21
+        var myShort: Short = 2001
+        var alsoMyShort = 21001
+        var myDouble: Double = 2010.99
+        var alsoMyDouble = 2912.82919
+        var myFloat: Float = 881.99F
+        var alsoMyfloat = 9.0293F
+
+        /**
+         * Variable conversion
+         */
+
+        var myConvertToInte = myFloat.toInt()
+
+        println(myConvertToInte)
+    }
