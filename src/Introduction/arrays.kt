e14baff7d2f4de057f6d871a99d26b6da9a90155
Okellothomas <thomasolal33@gmail.com>
2023-01-23 14:01:06 +0300

    add the myarraytype() method implemenation

--- src/Introduction/arrays.kt
+++ src/Introduction/arrays.kt
@@ -4,10 +4,15 @@ package Introduction
  * We do array implementation here
  */
 
+data class Students(val firstName: String, val lastName: String, val age: Int)
+
 fun main(){
     // we will call our methods here
 
     myArray()
+
+    // calling myarrayObject
+    myArrayType()
 }
 
 /**
@@ -19,7 +24,25 @@ private fun myArray(){
 
     var myArray = arrayOf("One", 2,3,4, "Sure",10)
 
+    // reasing values of an array
+    myArray[0] = "Okello"
+    myArray[1] = "Thomas"
+    myArray[2] = "Olal"
+    myArray[3] = "kadika"
     for (array in myArray){
         println(array)
+    }
+}
+
+private fun myArrayType(){
+    // array initialization
+
+    var myArray = arrayOf<Students>(Students("Okello", "Thomas", 8), Students("Olal", "Toma", 8),
+        Students("Jkuat", "firstyear", 11))
+
+    myArray[1] = Students("Tomas", "Tomas", 290)
+
+    for (student in myArray){
+        println(student)
     }
 }
